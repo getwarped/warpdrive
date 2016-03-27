aa2da63d8a7fe3918563fb4cfd216821aa967322
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-03-27 21:53:39 +1100

    Fix up OpenShift shell environments.

--- getwarped.sh
+++ getwarped.sh
@@ -69,3 +69,7 @@ find $APP_ROOT -type d -exec chmod g+ws {} \;
 
 find $APP_ROOT -perm 2755 -exec chmod g+w {} \;
 find $APP_ROOT -perm 0644 -exec chmod g+w {} \;
+
+if [ -f $APP_ROOT/etc/scl_enable ]; then
+    echo ". $APP_ROOT/warpdrive/etc/shell-init" >> $APP_ROOT/etc/scl_enable
+fi
