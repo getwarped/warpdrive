1ba449f9f58f0286e9380e318209d622723c25f5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-07-28 14:27:38 +1000

    Update version to 0.21.6.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def _files(prefix):
     return result
 
 setup(name = 'warpdrive',
-      version = '0.21.5',
+      version = '0.21.6',
       description = 'Launcher for Python web applications.',
       author = 'Graham Dumpleton',
       author_email = 'Graham.Dumpleton@gmail.com',
