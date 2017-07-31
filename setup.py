4d2d91a6fc497a973544653848886d62e9c850ae
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2017-07-31 20:05:55 +1000

    Increment version to 0.29.2.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ classifiers = [
 ]
 
 setup(name = 'warpdrive',
-      version = '0.29.1',
+      version = '0.29.2',
       description = 'Launcher for Python web applications.',
       long_description=long_description,
       url = 'https://github.com/GrahamDumpleton/warpdrive',
