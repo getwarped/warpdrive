9909b2b7e29c94752f18362392de0aa65a598747
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-10-06 19:12:09 +1100

    Indicate Python versions will support.

--- setup.py
+++ setup.py
@@ -19,10 +19,9 @@ classifiers = [
     'License :: OSI Approved :: BSD License',
     'Operating System :: MacOS :: MacOS X',
     'Operating System :: POSIX :: Linux',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Internet :: WWW/HTTP :: WSGI',
 ]
 
